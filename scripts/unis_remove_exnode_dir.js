8ae3792595554633360a5808c21a13b05e395b44
Ezra Kissel <ezkissel@indiana.edu>
2015-03-16 10:18:45 -0400

    Adding a mongo script to remove top-level exnode dir.

---
+++ scripts/unis_remove_exnode_dir.js
@@ -0,0 +1,20 @@
+function removeExnodes(e) {
+    files = db.exnodes.find({$and: [{mode: "file", parent: e.id}]})
+    files.forEach(function(f) { print("removing file: " + f.name); db.exnodes.remove(f)})
+    sdirs = db.exnodes.find({$and: [{mode: "directory"}, {parent: e.id}]})
+    sdirs.forEach(removeExnodes)
+    print("removing dir: " + e.name)
+    db.exnodes.remove(e)
+}
+
+REMOVE_DIR="GEC22"
+
+conn = new Mongo()
+db = conn.getDB("unis_db")
+now = new Date().getTime()*1000
+
+// find the top-level directory to recursively remove                                                                                           
+dir = db.exnodes.find({$and: [{name: REMOVE_DIR},
+                              {mode: "directory"},
+                              {parent: null}]})
+dir.forEach(removeExnodes)
