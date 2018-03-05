91c77a0c3aa3534416c7bed3dbfa2d692ffaf3d4
Jeremy Musser <jemusser@indiana.edu>
2018-03-05 13:08:33 -0500

    Added missing handler file

---
+++ periscope/handlers/abouthandler.py
@@ -0,0 +1,30 @@
+# =============================================================================
+#  periscope-ps (unis)
+#
+#  Copyright (c) 2012-2016, Trustees of Indiana University,
+#  All rights reserved.
+#
+#  This software may be modified and distributed under the terms of the BSD
+#  license.  See the COPYING file for details.
+#
+#  This software was created at the Indiana University Center for Research in
+#  Extreme Scale Technologies (CREST).
+# =============================================================================
+#!/usr/bin/env python
+
+import tornado.web
+import json
+
+from periscope.settings import MIME
+
+class AboutHandler(tornado.web.RequestHandler):
+    def initialize(self, **kwargs):
+        pass
+        
+    def get(self):
+        about = {
+            "uid": str(self.application.options["uuid"]),
+            "haschild": self.application.options["lookup"]
+        }
+        self.set_header("Content-Type", MIME["JSON"])
+        self.write(json.dumps(about, indent=4))
